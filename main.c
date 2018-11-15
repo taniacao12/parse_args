bb76f8dfba2f8af0f44bab1f3926eec3f11cb6ca
taniacao12 <tcaosu@stuy.edu>
2018-11-15 11:44:52 -0500

    fix errors

--- main.c
+++ main.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <signal.h>
 
 char ** parse_args( char * line ) {
-	char *s1 = line;
-  char ** ret;
-	while (s1) {
-		strcat(line, strsep(line, " "));
-	}
-	return ret;
+  char *s1 = calloc(strlen(line), 1);
+  char ** ret = calloc(6, sizeof(char *));
+  strcpy(s1, line);
+  for (int i = 0; s1; i++)
+    ret[i] = strsep(&s1, " ");
+  return ret;
 }
 
 int main() {
-	char line[100] = "woah-this-is-cool";
-	char *s1 = line;
-	printf("[%d]\n", strsep(&s1, "-"));
-	printf("line = [%s]\n", line);
-	printf("s1 = [%s]\n", s1);
+  char line[100] = "woah-this-is-cool";
+  char *s1 = line;
+  printf("[%s]\n", strsep(&s1, "-"));
+  printf("line = [%s]\n", line);
+  printf("s1 = [%s]\n", s1);
   
-	strcpy(line, "ls -a -l");
-	char ** args = parse_args(line);
-	execvp(args[0], args);
+  char ** args = parse_args("ls -a -l");
+  execvp(args[0], args);
 
   return 0;
 }
